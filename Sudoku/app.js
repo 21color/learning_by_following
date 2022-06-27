b47b55c4e8ea43354c3485e23d7961263dd2e06f
hanalee <lehhha0110@gmail.com>
2022-06-27 23:52:58 +0900

    let's go make sudoku

--- Sudoku/app.js
+++ Sudoku/app.js
@@ -8,6 +8,16 @@ for (let i = 0; i < squares; i++) {
 	inputElement.setAttribute('type', 'number');
 	inputElement.setAttribute('min', '1');
 	inputElement.setAttribute('max', '9');
+	if (
+		((i % 9 == 0 || i % 9 == 1 || i % 9 == 2) && i < 21) ||
+		((i % 9 == 6 || i % 9 == 7 || i % 9 == 8) && i < 27) ||
+		((i % 9 == 3 || i % 9 == 4 || i % 9 == 5) && i > 27 && i < 53) ||
+		((i % 9 == 0 || i % 9 == 1 || i % 9 == 2) && i > 53) ||
+		((i % 9 == 6 || i % 9 == 7 || i % 9 == 8) && i > 53)
+	) {
+		inputElement.classList.add('odd-section');
+	}
+
 	puzzleBorad.appendChild(inputElement);
 }
 
@@ -23,22 +33,36 @@ const joinvalue = () => {
 	console.log(submission);
 };
 
+const populateValue = (isSolvable, solution) => {
+	const inputs = document.querySelectorAll('input');
+	if (isSolvable && solution) {
+		inputs.forEach((input, i) => {
+			input.value = solution[i];
+		});
+	}
+};
 const solve = () => {
+	joinvalue();
+	const data = submission.join('');
+	console.log('data', data);
 	const options = {
 		method: 'POST',
 		url: 'https://solve-sudoku.p.rapidapi.com/',
 		headers: {
 			'content-type': 'application/json',
-			'X-RapidAPI-Key': '4e7302bee8mshb571af3777e51e4p1163b2jsn985e02d06a82',
+			'X-RapidAPI-Key': './environment.env.RAPID_API_KEY',
 			'X-RapidAPI-Host': 'solve-sudoku.p.rapidapi.com',
 		},
-		data: '{"puzzle":"2.............62....1....7...6..8...3...9...7...6..4...4....8....52.............3"}',
+		data: {
+			puzzle: data,
+		},
 	};
 
 	axios
 		.request(options)
 		.then(function (response) {
 			console.log(response.data);
+			populateValue(response.data.solvable, response.data.solution);
 		})
 		.catch(function (error) {
 			console.error(error.response);
